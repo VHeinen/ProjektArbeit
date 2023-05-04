be2e8b4ec84b92bf35e676e8d3028a6f98ce5a7c
Robeng98 <124875274+Robeng98@users.noreply.github.com>
2023-05-04 11:40:54 +0200

    Update swing.java

--- ProjektArbeit/src/swing.java
+++ ProjektArbeit/src/swing.java
@@ -1,6 +1,23 @@
-import javax.swing.*;  
+package ProjektArbeit;
+
+import java.awt.Color;
+import java.io.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.Graphics;
+
 public class swing {  
+	
+	private static JFrame unserFenster;
+	private static JLabel label;
+	
 	public static void main(String[] args) {  
-		JFrame unserFenster = new JFRame;
+		unserFenster = new JFrame();
+		unserFenster.setTitle("Notenübersicht");
+		unserFenster.setSize(600, 800);
+		JPanel pane = new JPanel();
+	    unserFenster.add(pane);
+		unserFenster.setVisible(true);
 	}
 }
