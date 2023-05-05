43994ce491f52e478870c702b197d6d51d6920ca
Robeng98 <124875274+Robeng98@users.noreply.github.com>
2023-05-05 14:59:32 +0200

    Update swing.java

--- ProjektArbeit/src/ProjektArbeit/swing.java
+++ ProjektArbeit/src/ProjektArbeit/swing.java
@@ -17,46 +17,60 @@ public class swing {
 	private static JLabel label;
 	
 	public static void main(String[] args) {
+		//JFrame erstellen, Button+Logik inklusive
+		window = buildJFrame();
+	}
+	
+	public static JFrame buildJFrame() {
+		
+		//JFrame aufbauen
 		window = new JFrame();
 		window.setTitle("Notenübersicht");
 		window.setSize(1080, 1920);
+		
+		//JPanel erstellen
 		JPanel pane = new JPanel();
+		
+		//Boxen zur Anordnung erstellen
 		Box vert = Box.createVerticalBox();
 		Box headlinesBox = Box.createHorizontalBox();
 		Box examBox = Box.createHorizontalBox();
 		Box epoBox = Box.createHorizontalBox();
 		Box otherBox = Box.createHorizontalBox();
 		Box footerBox = Box.createHorizontalBox();
+		
+		//Klassenarbeitsabschnitt
 		JLabel exam = new JLabel("Klassenarbeit:  ");
 		examBox.add(exam);
-		JLabel examGrade = new JLabel("                             Note     ");
+		JLabel examGrade = new JLabel("                             Note                   ");
 		headlinesBox.add(examGrade);
 		JTextField examGradeInputField = new JTextField("", 15);
 		examGradeInputField.setForeground(Color.BLUE);
 		examBox.add(examGradeInputField);
-		JLabel examWeighting = new JLabel("              Gewichtung              ");
+		JLabel examWeighting = new JLabel("              Gewichtung                      ");
 		headlinesBox.add(examWeighting);
 		JTextField examWeightingInputField = new JTextField("", 15);
 		examWeightingInputField.setForeground(Color.BLUE);
 		examBox.add(examWeightingInputField);
-		JLabel examComment = new JLabel("     Kommentar");
+		JLabel examComment = new JLabel("           Kommentar");
 		headlinesBox.add(examComment);
 		JTextField examCommentInputField = new JTextField("", 15);
 		examCommentInputField.setForeground(Color.BLUE);
 		examBox.add(examCommentInputField);
+		
+		//Klassenarbeitsbutton
 		JButton buttonExams = new JButton("Klassenarbeit speichern");
 		buttonExams.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String gradeToken;
-				float weightingToken;
-				String commentToken;
-				gradeToken = examGradeInputField.getText();
-				weightingToken = Float.parseFloat(examWeightingInputField.getText());
-				commentToken = examCommentInputField.getText();
+				String gradeToken = examGradeInputField.getText();
+				float weightingToken = Float.parseFloat(examWeightingInputField.getText());
+				String commentToken = examCommentInputField.getText();
 				//TODO: DB Update Tokens
 			}
 		});
 		footerBox.add(buttonExams);
+		
+		//Epochalnotenabschnitt
 		JLabel Epo = new JLabel("Epochalnote:    ");
 		epoBox.add(Epo);
 		JTextField epoGradeInputField = new JTextField("", 15);
@@ -68,19 +82,20 @@ public class swing {
 		JTextField epoCommentInputField = new JTextField("", 15);
 		epoCommentInputField.setForeground(Color.BLUE);
 		epoBox.add(epoCommentInputField);
+		
+		//Epochalnotenbutton
 		JButton buttonEpos = new JButton("Epochalnote speichern");
 		buttonEpos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String gradeToken;
-				float weightingToken;
-				String commentToken;
-				gradeToken = epoGradeInputField.getText();
-				weightingToken = Float.parseFloat(epoWeightingInputField.getText());
-				commentToken = epoCommentInputField.getText();
+				String gradeToken = epoGradeInputField.getText();
+				float weightingToken = Float.parseFloat(epoWeightingInputField.getText());
+				String commentToken = epoCommentInputField.getText();
 				//TODO: DB Update Tokens
 			}
 		});
 		footerBox.add(buttonEpos);
+		
+		//Andere Note Abschnitt
 		JLabel other = new JLabel("andere Note:    ");
 		otherBox.add(other);
 		JTextField otherGradeInputField = new JTextField("", 15);
@@ -92,26 +107,31 @@ public class swing {
 		JTextField otherCommentInputField = new JTextField("", 15);
 		otherCommentInputField.setForeground(Color.BLUE);
 		otherBox.add(otherCommentInputField);
+		
+		//Andere Note Button
 		JButton buttonOthers = new JButton("andere Note speichern");
 		buttonOthers.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String gradeToken;
-				float weightingToken;
-				String commentToken;
-				gradeToken = epoGradeInputField.getText();
-				weightingToken = Float.parseFloat(epoWeightingInputField.getText());
-				commentToken = epoCommentInputField.getText();
+				String gradeToken = epoGradeInputField.getText();
+				float weightingToken = Float.parseFloat(epoWeightingInputField.getText());
+				String commentToken = epoCommentInputField.getText();
 				//TODO: DB Update Tokens
 			}
 		});
 		footerBox.add(buttonOthers);
+		
+		//Boxen anordnen
 		vert.add(headlinesBox);
 		vert.add(examBox);
 		vert.add(epoBox);
 		vert.add(otherBox);
 		vert.add(footerBox);
 		pane.add(vert);
+		
+		//JFrame fertigmachen
 		window.add(pane);
 		window.setVisible(true);
+		
+		return(window);
 	}
 }
