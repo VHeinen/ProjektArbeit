052a90fcaff56fa3d247f76c92cd344b382ad53d
Robeng98 <124875274+Robeng98@users.noreply.github.com>
2023-06-09 17:44:45 +0200

    Update queries.java

--- ProjektArbeit/src/ProjektArbeit/queries.java
+++ ProjektArbeit/src/ProjektArbeit/queries.java
@@ -1,6 +1,7 @@
 package ProjektArbeit;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class queries {
 
@@ -18,9 +19,17 @@ public class queries {
 	}
 	
 	public static String[] getNamesFromSchoolClass() throws ClassNotFoundException, SQLException {
-		String classQuery = "select name from schoolclass";
-		String[] resultArr = database.getDataFromDatabase(classQuery, 1);
-		return resultArr;
+		String query = "select count(id) from schoolclass";
+		String[] countStr = database.getDataFromDatabase(query, 1);
+		int count = Integer.parseInt(countStr[0]);
+		String[] returnArr = new String[count];
+		String[] resultArr = new String[1];
+		for(int i = 0; i < count; i++) {
+			query = "select name from schoolclass" + " where id = '" + i + "'";
+			resultArr = database.getDataFromDatabase(query, 1);
+			returnArr[i] = resultArr[0];
+		}
+		return returnArr;
 	}
 	
 	public static String[] getSubjectNamesByTeacherID(int userID) throws ClassNotFoundException, SQLException {
@@ -38,6 +47,12 @@ public class queries {
 		return resultArr;
 	}
 	
+	public static String[] getIdFromPupilByName(String[] nameParts) throws ClassNotFoundException, SQLException {
+		String query = "select id from pupil where firstName = '" + nameParts[0] + "' and lastName = '" + nameParts[1] + "'";
+		String[] resultArr = database.getDataFromDatabase(query, 1);
+		return resultArr;
+	}
+	
 	public static String[] getPupilsFromPupil() throws NumberFormatException, ClassNotFoundException, SQLException {
 		String query = "select count(id) from pupil";
 		String[] countStr = database.getDataFromDatabase(query, 1);
@@ -45,10 +60,42 @@ public class queries {
 		String[] resultArr = new String[2];
 		String[] returnArr = new String[count];
 		for(int i = 0; i < count; i++) {
-			String pupilQuery = "select firstName, lastName from pupil where id = '" + i + "'";
-			resultArr = database.getDataFromDatabase(pupilQuery, 2);
+			query = "select firstName, lastName from pupil where id = '" + i + "'";
+			resultArr = database.getDataFromDatabase(query, 2);
 			returnArr[i] = resultArr[0] + " " + resultArr[1];
 		}
 		return returnArr;
+	}
+	
+	public static ArrayList<String> getGradeTypeDateTeacherSubjectByPupilId(int pupilID) throws ClassNotFoundException, SQLException {
+	    ArrayList<String> gradeList = new ArrayList<>();
+	    String query = "SELECT id, grade, type, date, teacherID, subjectID FROM grade WHERE pupilID = '" + pupilID + "' ORDER BY id ASC";
+	    String[][] resultArr = database.getAllDataFromDatabase(query, 6);
+	    
+	    if (resultArr != null && resultArr.length > 0) {
+	        for (String[] row : resultArr) {
+	            if (row[0] != null) {
+	                gradeList.add("Note: " + row[1] + " - Notenart: " + row[2] + " - Datum: " + row[3] + " - Lehrer: " + getTeacherNameFromTeacherByID(Integer.parseInt(row[4])) + " - Fach: " + getSubjectNameFromSubjectByID(Integer.parseInt(row[5])));
+	            }
+	        }
+	    }
+	    
+	    return gradeList;
+	}
+
+	public static String getTeacherNameFromTeacherByID(int id) throws ClassNotFoundException, SQLException {
+		String query = "SELECT lastName FROM teacher WHERE id = '" + id + "'";
+		return database.getDataFromDatabase(query, 1)[0];
+	}
+	
+	public static String getSubjectNameFromSubjectByID(int id) throws ClassNotFoundException, SQLException {
+		String query = "SELECT name FROM subject WHERE id = '" + id + "'";
+		return database.getDataFromDatabase(query, 1)[0];
+	}
+	
+	public static String[] getIdFromSubjectByName(String name) throws ClassNotFoundException, SQLException {
+		String query = "select id from subject where name = " + name;
+		String[] resultArr = database.getDataFromDatabase(query, 1);
+		return resultArr;
 	}
 }
