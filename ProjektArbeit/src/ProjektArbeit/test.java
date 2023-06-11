46e3f76a636ab0baca15cdf88e33ea26bea31f46
Robeng98 <124875274+Robeng98@users.noreply.github.com>
2023-06-11 12:58:17 +0200

    Update test.java

--- ProjektArbeit/src/ProjektArbeit/test.java
+++ ProjektArbeit/src/ProjektArbeit/test.java
@@ -10,14 +10,22 @@ import com.aspose.pdf.Page;
 public class test {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-
-	// - How to inserate in database - 
+// 		TODO Auto-generated method stub
 		
-//		database.writeSubjectInDatabase(0, "LF1");
+// 		How to insert in database
+		
+//		database.writePupilInDatabase(2, "TestUser", "TestUser", 0);
+//		database.writeTeacherInDatabase(1, "bla", "bla", 0, 2, 3);
+//		database.writeSchoolclassInDatabase(1, "1a", 0);
 //		database.writeSubjectInDatabase(1, "Englisch");
+//		database.writeGradeInDatabase(1, 5, 1.3f, "Test", Date.valueOf("2023-05-25"), 0, 0, 0);
+//		database.writeTeacherInDatabase(2, "TestUser", "TestUser", 0, 0, 0);
+//		database.writeSchoolclassInDatabase(2, "4c", 0);
 //		database.writeSubjectInDatabase(2, "LF2");
-//		database.writeSubjectInDatabase(3, "Lf3");
+//		database.writeGradeInDatabase(2, 3, 1.3f, "Test", Date.valueOf("2023-05-25"), 0, 0, 0);
+//		database.writePupilManagementInDatabase(1, "test", "null", 1);
+//		database.writeTeacherManagementInDatabase(1, "schach", "matt", 1);
+//	
 //		database.writeSubjectInDatabase(4, "Deutsch");
 //		database.writeSubjectInDatabase(5, "Mathe");
 //		database.writeSubjectInDatabase(6, "Reli");
@@ -36,33 +44,25 @@ public class test {
 //		database.writeSubjectInDatabase(19, "Sozialkunde");
 //		database.writeSubjectInDatabase(20, "Geschichte");
 //		
-//		database.writeSchoolclassInDatabase(0, "BSIT22a", 1);
-//		database.writeSchoolclassInDatabase(1, "1a", 0);
-//		database.writeSchoolclassInDatabase(1, "4c", 4);
 //		database.writeSchoolclassInDatabase(3, "TestClass", 0);
-//		
-//		database.writeTeacherInDatabase(0, "Test", "Test", 0, 0, 0);
-//		database.writeTeacherInDatabase(1, "Tara", "Thom", 0, 2, 3);
+		
 //		database.writeTeacherInDatabase(2, "Tom", "Tree", 5, 2, 0);
-//		database.writeTeacherInDatabase(3, "Ken", "Kankeki", 1, 2, 0);
+//		database.writeTeacherInDatabase(3, "teacher", "teacher", 1, 2, 0);
 //		database.writeTeacherInDatabase(4, "Hans", "Baum", 5, 3, 10);
 //		database.writeTeacherInDatabase(5, "Peter", "Müller", 10, 8, 3);
 //		database.writeTeacherInDatabase(6, "Johanna", "Tee", 4, 8, 20);
 //		database.writeTeacherInDatabase(7, "Beate", "Klaus", 20, 19, 1);
-//		database.writeTeacherInDatabase(8, "Angelika", "Raute", 1, 2, 3);
-//		database.writeTeacherInDatabase(9, "Florian", "Edgar", 1, 4, 7);
-//		database.writeTeacherInDatabase(10, "Jürgen", "Blub", 12, 13, 15);
+//		database.writeTeacherInDatabase(8, "vorname", "nachname", 1, 2, 3);
+//		database.writeTeacherInDatabase(9, "bla", "bla", 1, 4, 7);
+//		database.writeTeacherInDatabase(10, "test", "test", 12, 13, 15);
 //		
-//		database.writePupilInDatabase(0, "Test", "Test", 0);
-//		database.writePupilInDatabase(1, "Hans", "Olaf", 2);
-//		database.writePupilInDatabase(2, "Irene", "Plam", 0);
 //		database.writePupilInDatabase(3, "Harald", "Wurst", 2);
 //		database.writePupilInDatabase(4, "Java", "Itzz", 1);
 //		database.writePupilInDatabase(5, "Jason", "Wald", 1);
 //		database.writePupilInDatabase(6, "Franz", "Erhardt", 3);
 //		database.writePupilInDatabase(7, "Jessica", "Nutella", 3);
 //		database.writePupilInDatabase(8, "Petra", "Potsdamm", 1);
-//		database.writePupilInDatabase(9, "Günther", "Udo", 1);
+//		database.writePupilInDatabase(9, "Günther", "Tatatatata", 1);
 //		database.writePupilInDatabase(10, "Popo", "Lala", 3);
 //		database.writePupilInDatabase(11, "Olaf", "Snowman", 2);
 //		database.writePupilInDatabase(12, "Que", "Nyguen", 1);
@@ -75,101 +75,89 @@ public class test {
 //		database.writePupilInDatabase(19, "Willy", "Braun", 3);
 //		database.writePupilInDatabase(20, "Lara", "Udo", 1);
 //		database.writePupilInDatabase(21, "Anna", "Blatt", 2);
-//		database.writePupilInDatabase(22, "Gutfried", "Hye", 2);
+//		database.writePupilInDatabase(22, "Tu", "Hye", 2);
 //		database.writePupilInDatabase(23, "Zorro", "Zahn", 3);
 //		database.writePupilInDatabase(24, "Yuuki", "Hanabi", 1);
-//		database.writePupilInDatabase(25, "Bernd", "Hard", 2);	
+//		database.writePupilInDatabase(25, "Bernd", "Rasch", 2);	
 //		
-//		database.writeGradeInDatabase(0, 6, 2.0f, "Test", Date.valueOf("2025-05-20"), 0, 0, 0);
-//		database.writeGradeInDatabase(1, 6, 2.0f, "Test", Date.valueOf("2025-05-20"), 0, 0, 0);
-//		database.writeGradeInDatabase(2, 6, 2.0f, "Test", Date.valueOf("2023-05-08"), 12, 4, 3);
-//		database.writeGradeInDatabase(3, 1, 20.0f, "Klassenarbeit", Date.valueOf("2023-05-25"), 7, 6, 1);
-//		database.writeGradeInDatabase(4, 3, 4.0f, "Präsi", Date.valueOf("2023-02-24"), 1, 2, 6);
-//		database.writeGradeInDatabase(5, 4, 8.0f, "Test", Date.valueOf("2023-03-12"), 8, 8, 8);
-//		database.writeGradeInDatabase(6, 2, 15.0f, "Aufgaben Abgabe", Date.valueOf("2023-01-04"), 20, 10, 4);
+//		database.writeGradeInDatabase(8, 1, 1.0f, "Test", Date.valueOf("2023-05-08"), 3, 6, 3);
+//		database.writeGradeInDatabase(9, 1, 1.0f, "Klassenarbeit", Date.valueOf("2023-05-25"), 3, 6, 1);
+//		database.writeGradeInDatabase(10, 3, 1.0f, "Präsi", Date.valueOf("2023-02-24"), 3, 2, 6);
+//		database.writeGradeInDatabase(11, 4, 1.0f, "Test", Date.valueOf("2023-03-12"), 3, 8, 8);
+//		database.writeGradeInDatabase(12, 2, 1.0f, "Aufgaben Abgabe", Date.valueOf("2023-01-04"), 3, 10, 4);
 //		
-//		database.writePupilManagementInDatabase(0, "01test", "test", 0);
-//		database.writePupilManagementInDatabase(1, "01HansOlaf", "xyz123", 1);
-//		database.writePupilManagementInDatabase(2, "01IrenePalm", "123456789", 2);
 //		
-//		database.writeTeacherManagementInDatabase(0, "00test", "test", 0);
-//		database.writeTeacherManagementInDatabase(1, "00TaraThom", "Ibims1Teacher", 1);
-//		database.writeTeacherManagementInDatabase(2, "00TomTree", "Baum123", 2);
-		
-		
-		
-		// - How to get data from database -
-	
+		// How to get data from database
+//	
 //		String[] resultArr = database.getDataFromDatabase("select * from pupil where id = 4", 4);
 //		for (String elements : resultArr) {
 //			System.out.println(elements);
 //		}
-//		
-//		String queryy = "select * from teacher where id = 0 and lastName = 'Test'";
-//		String[] test = database.getDataFromDatabase(queryy, 6);
-//		
-//		String[] testi = database.getDataFromDatabase(queries.getAllDataFromPupilByID(1), 4);
-//		String[] testt = database.getDataFromDatabase(queries.getAllDataFromPupilByFirstName("Test"), 4);
-//		String queryyy = "select count(id) from pupil";
-//		String[] testtt = database.getDataFromDatabase(queryyy, 1);
+		
+		//String queryy = "select * from teacher where id = 0 and lastName = 'Test'";
+		//String[] test = database.getDataFromDatabase(query, 6);
+		
+//		String[] test = database.getDataFromDatabase(Queries.getAllDataFromPupilByID(1), 4);
+//		String[] test = database.getDataFromDatabase(queries.getAllDataFromPupilByFirstName("Test"), 4);
+//		String queryy = "select count(id) from pupil";
+//		String[] test = database.getDataFromDatabase(queryy, 1);
 //		
 //		
 //		for (String elements : test) {
 //			System.out.println(elements);
 //		}
-		
-		
-		
-		
-		//  - How to change Data from database -
-		
-//		String tableName = "pupil";
-//		String columnName = "firstName";
-//		int id = 22;
-//		String change = "Gutfried";
+//		
+//		
+//		// How to change Data from database
+//		
+//		String tableName = "teachermanagement";
+//		String columnName = "passwort";
+//		int id = 1;
+//		String change = "matt";
 //		
 //		database.changeDataInDatabaseByTableNameAndColumnName(tableName, columnName, id, change);
-		
-		
-		
-		// - How to create pdf -
-		
-//		Document document = pdf.document;
-//		Page page = pdf.page;
-//		page = pdf.setTitleForPDF(page);
 //		
+//		
+//		
+//		// How to create pdf 
+//
+		Document document = pdf.document;
+		Page page = pdf.page;
+		
+		page = pdf.setTitleForPDF(page);
+//		document = pdf.createTableForPDFBytableName(document, "subject");
 //		document = pdf.createTableForPDFBytableName(document, "pupil");
 //		document = pdf.createWhitespaceForNewTables(document);
 //		
 //		document = pdf.createTableForPDFBytableName(document, "subject");
 //		document = pdf.createWhitespaceForNewTables(document);
 //		
-//		String query;
+		String query;
 //	
 //		 query = "select id from pupil where id = 5";
-//		document = pdf.createTableForPDFByQuery(document, page, query, 1);
+//		document = pdf.createTableForPDFByQuery(document, query, 1);
 //		
 //		document = pdf.createWhitespaceForNewTables(document);
 //		
 //		 query = "select id, firstName from pupil where id = 5";
-//		document = pdf.createTableForPDFByQuery(document, page, query, 2);
+//		document = pdf.createTableForPDFByQuery(document, query, 2);
 //		
 //		document = pdf.createWhitespaceForNewTables(document);
 //		
 //		
 //		 query = "select id, firstName, lastName from pupil where id = 5";
 //		document = pdf.createTableForPDFByQuery(document, query, 3);
-//		
-//		document = pdf.createWhitespaceForNewTables(document);
-//		document = pdf.createWhitespaceForNewTables(document);
-//		document = pdf.createWhitespaceForNewTables(document);
-//		
-//		query = "select id, firstName, lastName, classID from pupil where id = 5";
-//		document = pdf.createTableForPDFByQuery(document, query, 4);
-//		
-//		document = pdf.createWhitespaceForNewTables(document);	
-//		
-//		
+		
+		document = pdf.createWhitespaceForNewTables(document);
+		document = pdf.createWhitespaceForNewTables(document);
+		document = pdf.createWhitespaceForNewTables(document);
+		
+		query = "select id, firstName, lastName, classID from pupil where id = 5";
+		document = pdf.createTableForPDFByQuery(document, query, 4);
+		
+		document = pdf.createWhitespaceForNewTables(document);	
+		
+		
 //		query = "select id, grade, weighting, type, date from grade where id = 2";
 //		document = pdf.createTableForPDFByQuery(document, query, 5);
 //		
@@ -187,20 +175,20 @@ public class test {
 //		
 //		query = "select id, grade, weighting, type, date, pupilID, teacherID, subjectID from grade where id = 2";
 //		document = pdf.createTableForPDFByQuery(document, query, 8);
-//		
-//		document = pdf.createWhitespaceForNewTables(document);
-//		
-//		
-//		document = pdf.createTableForPDFBytableName(document, "teacher");
-//		document = pdf.createWhitespaceForNewTables(document);
-//		
-//	
-//		
-//		pdf.saveDocument(document, "Notenübersicht.pdf");
+		
+		document = pdf.createWhitespaceForNewTables(document);
 		
 		
+		document = pdf.createTableForPDFBytableName(document, "teacher");
+		document = pdf.createWhitespaceForNewTables(document);
 		
+	
 		
+		pdf.saveDocument(document, "Notenübersicht.pdf");
+//		
+//		
+//		
+//		
 	}
 
 }
